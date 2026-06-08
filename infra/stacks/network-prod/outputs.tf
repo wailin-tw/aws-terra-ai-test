2a3bdc24aad1882366206b4e4bd9991d831ac154
wailin-tw <wailin.s@trinitywizards.com>
2026-06-08 15:26:39 +0800

    infra: add network-prod stack

---
+++ infra/stacks/network-prod/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID for prod environment"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "internet_gateway_id" {
+  description = "Internet gateway ID"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "NAT gateway ID, null if NAT is disabled"
+  value       = module.vpc.nat_gateway_id
+}
---
+++ infra/stacks/network-prod/variables.tf
@@ -0,0 +1,73 @@
+variable "aws_region" {
+  description = "AWS region for the stack"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Short project identifier used in naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "owner" {
+  description = "Resource owner tag"
+  type        = string
+}
+
+variable "cost_center" {
+  description = "Cost center tag"
+  type        = string
+}
+
+variable "application" {
+  description = "Application tag"
+  type        = string
+}
+
+variable "data_classification" {
+  description = "Data classification tag"
+  type        = string
+  default     = "internal"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "vpc_cidr must be a valid IPv4 CIDR block."
+  }
+}
+
+variable "availability_zones" {
+  description = "Availability zones for subnet distribution"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR list"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR list"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT gateway for private subnet egress"
+  type        = bool
+  default     = true
+}
+
+variable "additional_tags" {
+  description = "Optional extra tags"
+  type        = map(string)
+  default     = {}
+}
